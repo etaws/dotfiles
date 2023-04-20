08f63c9b5fa139762d7354dae40645dab7a39031
etaws <etawsz@gmail.com>
2023-04-20 19:55:38 +0800

    change size of toggle term

--- envim/.config/nvim/lua/plugins/toggleterm.lua
+++ envim/.config/nvim/lua/plugins/toggleterm.lua
@@ -53,7 +53,7 @@ return {
       local htop = Terminal:new({
         cmd = "htop",
         dir = "git_dir",
-        direction = "vertical",
+        direction = "float",
         float_opts = {
           border = "double",
         },
@@ -67,7 +67,7 @@ return {
       vim.api.nvim_set_keymap(
         "n",
         "<leader>cg",
-        '<cmd>TermExec cmd="cargo test" size=10 dir=git_dir direction=horizontal<CR>',
+        '<cmd>TermExec cmd="cargo test" size=80 dir=git_dir direction=vertical<CR>',
         { noremap = true, silent = true }
       )
     end,
