7d83e9aa4a180375bb250831ca158cf4020198f6
etaws <etawsz@gmail.com>
2024-03-27 17:07:59 +0800

    use bufferline now
    more bufferline settings

--- envim/.config/nvim/lua/plugins/bufferline.lua
+++ envim/.config/nvim/lua/plugins/bufferline.lua
@@ -6,6 +6,35 @@ return {
     dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
+    opts = {
+      options = {
+        mode = "buffers", -- set to "tabs" to only show tabpages instead
+        numbers = "ordinal", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+        indicator = {
+          icon = "▎", -- this should be omitted if indicator style is not 'icon'
+          style = "icon", -- 'icon' | 'underline' | 'none',
+        },
+        buffer_close_icon = "",
+        modified_icon = "●",
+        close_icon = "",
+        left_trunc_marker = "",
+        right_trunc_marker = "",
+        max_name_length = 18,
+        max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+        tab_size = 18,
+        truncate_names = true, -- whether or not tab names should be truncated
+        show_buffer_icons = true,
+        show_buffer_close_icons = true,
+        show_close_icon = true,
+        show_tab_indicators = true,
+        persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+        -- can also be a table containing 2 custom separators
+        -- [focused and unfocused]. eg: { '|', '|' }
+        separator_style = "slant", -- | "thick" | "thin" | { 'any', 'any' },
+        enforce_regular_tabs = true,
+        always_show_bufferline = true,
+      },
+    },
     keys = {
       { "<leader>1", "<cmd>BufferLineGoToBuffer 1<CR>" },
       { "<leader>2", "<cmd>BufferLineGoToBuffer 2<CR>" },
