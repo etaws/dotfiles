320d7c3b02acefea60c5529dae11adba9f5a6334
etaws <etawsz@gmail.com>
2025-04-10 19:10:14 +0800

    float on aerial

--- envim/.config/nvim/lua/plugins/aerial.lua
+++ envim/.config/nvim/lua/plugins/aerial.lua
@@ -10,15 +10,15 @@ return {
     },
     config = function()
       require("aerial").setup({
-        -- opt aionally use on_attach to set keymaps when aerial has attached to a buffer
-        on_attach = function(bufnr)
-          -- Jump forwards/backwards with '{' and '}'
-          -- vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-          -- vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-        end,
+        backends = { "lsp" },
+        layout = {
+          max_width = { 120, 0.5 },
+          width = 80,
+          min_width = 10,
+        },
       })
       -- You probably also want to set a keymap to toggle aerial
-      vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
+      vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle float<CR>")
     end,
   },
 }
